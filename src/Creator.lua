b75152694e1cb240e158edc352992c00467082d2
PapaDusty <cyknseppuku@gmail.com>
2025-10-25 00:14:45 -0700

    Update Creator.lua

--- src/Creator.lua
+++ src/Creator.lua
@@ -1,1 +1,30 @@
+-- Simple Creator utility for Serenity UI
+local Creator = {}
 
+function Creator.New(className, properties, children)
+    local instance = Instance.new(className)
+    
+    if properties then
+        for property, value in pairs(properties) do
+            if property == "Parent" then
+                instance.Parent = value
+            else
+                instance[property] = value
+            end
+        end
+    end
+    
+    if children then
+        for _, child in ipairs(children) do
+            child.Parent = instance
+        end
+    end
+    
+    return instance
+end
+
+function Creator.AddSignal(signal, callback)
+    return signal:Connect(callback)
+end
+
+return Creator
