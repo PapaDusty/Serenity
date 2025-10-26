24daa3716bbd4166808a72d512f1ddbc3bef4b1f
PapaDusty <cyknseppuku@gmail.com>
2025-10-26 00:13:31 -0700

    Update Button.lua

--- src/Components/Button.lua
+++ src/Components/Button.lua
@@ -1,1 +1,14 @@
+-- Button component for Serenity UI
+return function(Title, Parent)
+    local Serenity = require(script.Parent.Parent)
+    
+    local Button = {
+        Type = "Button",
+        Title = Title
+    }
 
+    -- Button implementation would go here
+    -- This ensures the component exists
+    
+    return Button
+end
