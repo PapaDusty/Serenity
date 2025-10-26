c0b54d1c5b3776231348faec03cbee93360ff1e0
PapaDusty <cyknseppuku@gmail.com>
2025-10-26 00:13:21 -0700

    Update Tab.lua

--- src/Components/Tab.lua
+++ src/Components/Tab.lua
@@ -1,1 +1,15 @@
+-- Tab component for Serenity UI
+return function(Title, Icon, Parent, Window)
+    local Serenity = require(script.Parent.Parent)
+    
+    local Tab = {
+        Type = "Tab",
+        Title = Title,
+        Sections = {}
+    }
 
+    -- Tab implementation would go here
+    -- This ensures the component exists
+    
+    return Tab
+end
