32d6bdf1c80fa79c0e95ac4560c262272b83bb95
PapaDusty <cyknseppuku@gmail.com>
2025-10-26 19:30:34 -0700

    Create Assets.lua

---
+++ src/Assets.lua
@@ -0,0 +1,24 @@
+return {
+    ["Synapse"] = "118034688779559", -- Done
+    ["KRNL"] = "118034688779559", -- Done
+    ["JJSploit"] = "118034688779559", -- Done
+    ["Sentinel"] = "118034688779559", -- Failed
+    ["Delta"] = "118034688779559", -- Done
+    ["Zenith"] = "118034688779559", -- Done
+    ["Volcano"] = "118034688779559", -- Done
+    ["Velocity"] = "118034688779559", -- Done
+    ["Swift"] = "118034688779559", -- Done
+    ["Seliware"] = "118034688779559", -- Done
+    ["Valex"] = "118034688779559", -- Done
+    ["Potassium"] = "118034688779559", -- Done
+    ["Solara"] = "118034688779559", -- Done
+    ["Xeno"] = "118034688779559", -- Done
+    ["bunni"] = "118034688779559", -- Failed
+    ["SirHunt"] = "118034688779559", -- Failed
+    ["Hydrogen"] = "118034688779559", -- Failed
+    ["Codex"] = "118034688779559", -- Failed
+    ["Cryptic"] = "118034688779559", -- Failed
+    ["Opiumware"] = "118034688779559", -- Failed
+    ["Macsploit"] = "118034688779559", -- Failed
+    ["WeAreDevs"] = "118034688779559", -- Failed
+}
